a91a2d4abc8ec21d27c9a71978e57b2b42ec524c
ashwinj <ashwin.jandhyala@gmail.com>
2023-11-18 14:07:49 -0500

    App.jsx updates

---
+++ components/LandingPage.jsx
@@ -0,0 +1,16 @@
+import React, {useState} from 'react';
+
+// Import css
+import './RoadMap';
+
+// Road map component
+const LandingPage = () => {
+
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default RoadMap;
