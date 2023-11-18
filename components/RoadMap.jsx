47693d476299bdfb486161c63f8b52952346f0f4
amsierco <75064473+amsierco@users.noreply.github.com>
2023-11-18 16:07:04 -0500

    removed node_modules

--- components/RoadMap.jsx
+++ components/RoadMap.jsx
@@ -1,19 +1,51 @@
-import React, {useState} from 'React';
+import React, {useState} from 'react';
+
+// Import components
+import './Node';
 
 // Import css
-import './RoadMap';
+import './RoadMap.css';
 
 // Road map component
 const RoadMap = () => {
+    
+    const nodes = [
+        {
+            id: 1,
+            title: 'title',
+            data: 'data',
+        },
+        {
+            id: 2,
+            title: 'title2',
+            data: 'data2',
+        },
+        {
+            id: 3,
+            title: 'title3',
+            data: 'data3',
+        }
+    ]
+
+    // Array of lessons from API
+    // const [nodes, setnotes] = useState([]);
 
     return (
         <div className='roadmap-container'>
             <h2>Your roadmap</h2>
-            <div className='node-container'>
-
+            <div className='node-wrapper'>
+                <ul>
+                {nodes.map(node => { /* Loop over all nodes and return node comp */
+                    return (
+                        <li id={node.id}>
+                        <Node data={node}/>
+                        </li>
+                    )
+                })}
+                </ul>
             </div>
             <div className='practice-container'>
-{/* test */}
+
             </div>
         </div>
     );
