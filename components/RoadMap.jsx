bc06158383c801ac620a31d60f756ada979e95cf
amsierco <75064473+amsierco@users.noreply.github.com>
2023-11-18 13:46:37 -0500

    Refactored init

---
+++ components/RoadMap.jsx
@@ -0,0 +1,16 @@
+import React, {useState} from 'React';
+
+// Import css
+import './RoadMap';
+
+// Road map component
+const RoadMap = () => {
+
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default RoadMap;
